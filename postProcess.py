f27a1f093f7b3d7d3e07c9ea685601c9a41da78b
Chintan2108 <chintanmaniyar@gmail.com>
2021-12-01 23:46:33 -0500

    local processes added

--- postProcess.py
+++ postProcess.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageDraw, ImageFont
 import os
+import pandas as pd
 
 class PostProcess:
     '''
@@ -8,7 +9,7 @@ class PostProcess:
     # constructor 
     def __init__(self, ccdImages, ccdValues, GIFSpeed, outputDir):
         self.outpath = outputDir
-        self.ccdImages = ccdImages
+        self.ccdImagesDir = ccdImages
         self.ccdValues = ccdValues
         self.fps = GIFSpeed
     
@@ -16,4 +17,40 @@ class PostProcess:
         '''
         This function: 1. adds text label of image date to individial ccd image; 2. stitches them together into a gif
         '''
+        # reading the images
+        img_names = os.listdir(self.ccdImagesDir)
+        img_names = list(filter(lambda x: x.endswith('png'), img_names))
+        img_names = sorted(img_names, key = lambda x: int(x.split('_')[0]))
+
+        images = []
+        for img_name in img_names:
+            # read the image
+            img = Image.open(os.path.join(self.ccdImagesDir, img_name))
+
+            # add the label
+            d = ImageDraw.Draw(img)
+            fontParams = ImageFont.truetype('C:/Windows/Fonts/ARLRDBD.ttf', size=50)
+            label = img_name.split('_')[1] + ' ' + img_name.split('_')[-1].split('.')[0]
+            d.text((200,600), label, font=fontParams, fill=(0,0,0))
+
+            # append the labelled image
+            images.append(img)
         
+        # save all labelled images as GIF
+        images[0].save(fp = os.path.join(self.outpath, 'TSE_Animation.gif'), format = 'GIF', append_images = images, 
+                       save_all = True, duration = self.fps*1000)
+        
+    def createTSPlot(self):
+        '''
+        Reads the ccd values and saves a timeseries plot 
+        '''
+        # read ccd values
+        df = pd.read(self.ccdValues)
+
+        # plot the timeseries
+        ax = df.plot(df.keys()[0], df.keys()[1])
+        ax.set_ylabel('Cyano_Cells/L')
+        TSE_plot = ax.get_figure()
+
+        # save the timeseries on local disk
+        TSE_plot.savefig(os.path.join(self.outpath, 'TSE_Timeseries_Plot.png'))
